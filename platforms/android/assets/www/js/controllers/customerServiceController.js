8a40d84044dc52a6082d7ec8a3f785a355207b24
unknown <steven.xu@CHNSHAD140023.internal.sungard.corp>
2015-09-17 15:02:56 +0800

    remove plugins and platforms folders

--- platforms/android/assets/www/js/controllers/customerServiceController.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by seven on 5/2/2015.
- */
-
-var insight = angular.module('insight');
-insight.controller('CustomerServiceController', function($scope,$stateParams) {
-    console.log($stateParams.status);
-    alert($stateParams.status+"--------------------here is the one--------------------------");
-})
