44bc461086c03be1290732cf3596cf6556d04149
Steven Xu <nonumber1989@gmail.com>
2015-09-22 21:57:32 +0800

    borrowd from and to

--- www/js/controllers/booksForBorrowController.js
+++ www/js/controllers/booksForBorrowController.js
@@ -2,6 +2,8 @@
  * Created by seven on 8/23/2015.
  */
 var insight = angular.module('insight');
-insight.controller('BooksForBorrowController', function ($scope) {
-
+insight.controller('BooksForBorrowController', function ($scope,Account) {
+    Account.get(function(account){
+        $scope.account = account;
+    })
 })
