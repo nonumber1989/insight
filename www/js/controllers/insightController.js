a00cd704d2576d355ae3cbcfe392deb0f4499f6d
nonumber1989 <nonumber1989@gmail.com>
2016-02-24 17:44:32 +0800

    make little changes and test

--- www/js/controllers/insightController.js
+++ www/js/controllers/insightController.js
@@ -4,27 +4,27 @@
 
 var insight = angular.module('insight');
 
-insight.controller('InsightController', function ($scope, $ionicModal, $timeout, $localStorage, $translate, AccountService,Language) {
-        $scope.entries = [];
+insight.controller('InsightController', function($scope, $ionicModal, $timeout, $localStorage, $translate, AccountService, Language) {
+    $scope.entries = [];
 
     $scope.$storage = $localStorage;
 
-    Language.query(function(result){
+    Language.query(function(result) {
         $scope.languages = result;
-    },function(error){
+    }, function(error) {
 
     })
 
-        $scope.user = AccountService.getAccount();
+    $scope.user = AccountService.getAccount();
 
-    $scope.changeLanguage = function (key) {
+    $scope.changeLanguage = function(key) {
         $translate.use(key);
     };
     $scope.account = {
         language: 'zh_CN'
     };
-    $scope.getLanguageText = function (value) {
-        return _.result(_.find($scope.languages, function (theOne) {
+    $scope.getLanguageText = function(value) {
+        return _.result(_.find($scope.languages, function(theOne) {
             return theOne.value == value;
         }), 'text');
     }
@@ -35,27 +35,27 @@ insight.controller('InsightController', function ($scope, $ionicModal, $timeout,
     // Create the login modal that we will use later
     $ionicModal.fromTemplateUrl('templates/login.html', {
         scope: $scope
-    }).then(function (modal) {
+    }).then(function(modal) {
         $scope.modal = modal;
     });
 
     // Triggered in the login modal to close it
-    $scope.closeLogin = function () {
+    $scope.closeLogin = function() {
         $scope.modal.hide();
     };
 
     // Open the login modal
-    $scope.login = function () {
+    $scope.login = function() {
         $scope.modal.show();
     };
 
     // Perform the login action when the user submits the login form
-    $scope.doLogin = function () {
+    $scope.doLogin = function() {
         console.log('Doing login', $scope.loginData);
 
         // Simulate a login delay. Remove this and replace with your login
         // code if using a login system
-        $timeout(function () {
+        $timeout(function() {
             $scope.closeLogin();
         }, 1000);
     };
